ea1c3142f8c7f7d0af036f4962c0b3ea9a8908c7
rky1871998 <rky1871998>
2023-03-02 14:06:36 -0800

    fifth change

---
+++ demo.js
@@ -0,0 +1,6 @@
+var seconditem=document.querySelector
+('.list-group-item:nth.child(2)');
+seconditem.style.color='green';
+
+var submit=document.querySelector('input[type="submit"]');
+submit.value="SEND"
