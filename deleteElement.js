acff2bad116941939b880ceb1acbcc958234ee49
rky1871998 <rky1871998>
2023-03-19 04:23:31 -0700

    deleteElementFromlocalStorage

---
+++ deleteElement.js
@@ -0,0 +1,32 @@
+function saveData(event){
+    event.preventDefault();
+    const name=event.target.username.value;
+    const email=event.target.emailId.value;
+    const phonenumber=event.target.phonenumber.value;
+const obj={
+    name,
+    email,
+    phonenumber
+
+}
+localStorage.setItem(obj.email, JSON.stringify(obj))
+showOnscreen(obj)
+}
+function showOnscreen(obj){
+    const parentElem=document.getElementById("listOfitems")
+    //parentElem.innerHTML=parentElem.innerHTML + `<li> ${obj.name} - ${obj.email} - ${obj.phonenumber}</li>`
+    const childElem=document.createElement("li")
+    childElem.textContent=obj.name + ' - ' + obj.emaii + ' - ' + obj.phonenumber
+    
+    const deleteButton=document.createElement('input')
+    deleteButton.type="button"
+    deleteButton.value="Delete"
+    deleteButton.onclick=()=>{
+        localStorage.removeItem(obj.email)
+        parentElem.removeChild(childElem)
+    }
+    childElem.appendChild(deleteButton)
+    parentElem.appendChild(childElem)
+
+}
+
